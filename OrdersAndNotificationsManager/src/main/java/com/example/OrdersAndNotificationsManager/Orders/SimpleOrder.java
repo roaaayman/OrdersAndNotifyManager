d645bddbf661056944dc8b76244a969515207345
Samaselim1 <125610375+Samaselim1@users.noreply.github.com>
2023-12-25 23:20:44 +0200

    Simple order

--- OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Orders/SimpleOrder.java
+++ OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Orders/SimpleOrder.java
@@ -1,16 +1,45 @@
 package com.example.OrdersAndNotificationsManager.Orders;
 
 import com.example.OrdersAndNotificationsManager.Orders.Order;
+import com.example.OrdersAndNotificationsManager.Products.Products;
+
+import java.util.List;
 
 public class SimpleOrder implements Order {
-    private String productName;
+    private String orderId;
+    private String customerId;
+    private List<Products> products;
+    private boolean shipped;
+    private double shippingFees;
 
-    public SimpleOrder(String productName) {
-        this.productName = productName;
+    // Constructor
+    public SimpleOrder(String orderId, String customerId, List<Products> products) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.products = products;
+        this.shipped = false;
+        this.shippingFees = 0.0;
     }
 
     @Override
     public void placeorder() {
 
     }
+
+    public double calculateTotalPrice() {
+        double totalPrice = 0.0;
+        for (Products product : products) {
+            totalPrice += product.getPrice();
+        }
+        return totalPrice;
+    }
+
+    public void setShippingFees(double fees) {
+        this.shippingFees = fees;
+    }
+
+    public void markAsShipped() {
+        this.shipped = true;
+    }
+
 }
