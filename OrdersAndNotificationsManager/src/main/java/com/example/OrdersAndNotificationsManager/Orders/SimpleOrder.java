111bd6e89688490d3bf1fad5d566f9b54b5c0893
Samaselim1 <125610375+Samaselim1@users.noreply.github.com>
2023-12-25 23:33:43 +0200

    Simple Order update

--- OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Orders/SimpleOrder.java
+++ OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Orders/SimpleOrder.java
@@ -1,45 +1,45 @@
 package com.example.OrdersAndNotificationsManager.Orders;
 
-import com.example.OrdersAndNotificationsManager.Orders.Order;
+import com.example.OrdersAndNotificationsManager.Customers.Customer;
 import com.example.OrdersAndNotificationsManager.Products.Products;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleOrder implements Order {
-    private String orderId;
-    private String customerId;
+    private Customer customer;
     private List<Products> products;
-    private boolean shipped;
-    private double shippingFees;
+    private double shippingFee;
+
 
     // Constructor
-    public SimpleOrder(String orderId, String customerId, List<Products> products) {
-        this.orderId = orderId;
-        this.customerId = customerId;
-        this.products = products;
-        this.shipped = false;
-        this.shippingFees = 0.0;
+    public SimpleOrder(Customer customer) {
+        this.customer = customer;
+        this.products = new ArrayList<>();
+        this.shippingFee = 0.0;
     }
 
     @Override
     public void placeorder() {
-
+        double total = calculateTotal();
+        customer.setBalance(customer.getBalance() - total);
+        System.out.println("Simple order placed!");
     }
 
-    public double calculateTotalPrice() {
-        double totalPrice = 0.0;
+    public void addProduct(Products product) {
+        products.add(product);
+    }
+
+    public void setShippingFee(double shippingFee) {
+        this.shippingFee = shippingFee;
+    }
+
+    public double calculateTotal() {
+        double total = 0.0;
         for (Products product : products) {
-            totalPrice += product.getPrice();
+            total += product.getPrice();
         }
-        return totalPrice;
-    }
-
-    public void setShippingFees(double fees) {
-        this.shippingFees = fees;
-    }
-
-    public void markAsShipped() {
-        this.shipped = true;
+        return total + shippingFee;
     }
 
 }
