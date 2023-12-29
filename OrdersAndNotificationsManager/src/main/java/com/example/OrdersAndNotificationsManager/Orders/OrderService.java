8066d512a01a892ee9c3bdcaca01f24a7396edf5
rawanntarek <rawaaan245@gmail.com>
2023-12-29 09:35:24 +0200

    .

--- OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Orders/OrderService.java
+++ OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Orders/OrderService.java
@@ -2,6 +2,7 @@ package com.example.OrdersAndNotificationsManager.Orders;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,5 +13,17 @@ public class OrderService {
         // Pass the list of product names to the order
         String result = order.placeorder(productNames);
         return result;
+    }
+    public List<String> listOrdersForCustomer(String customerEmail) {
+        List<String> orders = new ArrayList<>();
+        List<String> simpleOrders = listSimpleOrdersForCustomer(customerEmail);
+        orders.addAll(simpleOrders);
+        List<String> compoundOrders = listCompoundOrdersForCustomer(customerEmail);
+        orders.addAll(compoundOrders);
+         return orders;
+    }
+    public List<String> listSimpleOrdersForCustomer(String customerEmail) {
+        List<String> simpleOrders = new ArrayList<>();
+        return simpleOrders;
     }
 }
