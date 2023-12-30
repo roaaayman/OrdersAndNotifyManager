0fff6406e2a58878508fae00e07dd0fcd10e65b0
rawanntarek <rawaaan245@gmail.com>
2023-12-30 13:55:31 +0200

    .

--- OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Customers/Customer.java
+++ OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Customers/Customer.java
@@ -13,6 +13,16 @@ public class Customer {
     private double balance;
     private String location;
     private String address;
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum;
+    }
+
+    private String phonenum;
     private List<SimpleOrder> simpleOrders = new ArrayList<>();
     private List<CompoundOrder> compoundOrders = new ArrayList<>();
 
@@ -33,11 +43,12 @@ public class Customer {
         this.location = location;
     }
 
-    public Customer(String email, String password,  String location,String address) {
+    public Customer(String email, String password,  String location,String address,String phonenum) {
         this.password=password;
         this.email=email;
         this.location=location;
         this.address=address;
+        this.phonenum=phonenum;
     }
 
     public String getEmail() {
--- OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Notifications/NotificationController.java
+++ OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Notifications/NotificationController.java
@@ -1,10 +1,14 @@
 package com.example.OrdersAndNotificationsManager.Notifications;
 
+import com.example.OrdersAndNotificationsManager.Customers.Customer;
+import com.example.OrdersAndNotificationsManager.Customers.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,20 +19,47 @@ public class NotificationController {
     private final EmailLNotificationObserver emailNotificationObserver;
 
     @Autowired
+    CustomerService customerService;
     public NotificationController(SMSNotificationObserver smsNotificationObserver, EmailLNotificationObserver emailNotificationObserver) {
         this.smsNotificationObserver = smsNotificationObserver;
         this.emailNotificationObserver = emailNotificationObserver;
     }
 
-    @GetMapping("/sms-notifications")
-    public List<String> getSMSNotifications() {
-        // Retrieve SMS notifications from the SMS observer
-        return smsNotificationObserver.getNotifications();
+    @GetMapping("/sms-notifications/{phonenum}")
+    public List<String> getSMSNotifications(@PathVariable String phonenum) {
+
+        List<String> allSMSNotifications = smsNotificationObserver.getNotifications();
+        List<String> cutomerSMSnotifications = new ArrayList<>();
+        Customer customer=customerService.getCustomerByPhone(phonenum);
+
+        if(customer!=null) {
+            for (String notification : allSMSNotifications) {
+                if (notification.contains(customer.getEmail())) {
+                    cutomerSMSnotifications.add(notification);
+                }
+            }
+        }
+
+        return cutomerSMSnotifications;
+
     }
 
-    @GetMapping("/email-notifications")
-    public List<String> getEmailNotifications() {
-        // Retrieve email notifications from the email observer
-        return emailNotificationObserver.getNotifications();
+    @GetMapping("/email-notifications/{email}")
+    public List<String> getEmailNotificationsForCustomer(@PathVariable String email) {
+
+        List<String> allEmailNotifications = emailNotificationObserver.getNotifications();
+        List<String> customerEmailNotifications = new ArrayList<>();
+
+        Customer customer=customerService.getCustomerByEmail(email);
+        if(customer!=null) {
+            for (String notification : allEmailNotifications) {
+                if (notification.contains(email)) {
+                    customerEmailNotifications.add(notification);
+                }
+            }
+        }
+
+        return customerEmailNotifications;
     }
+
 }
